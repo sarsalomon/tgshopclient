b9a996f82b27ac07327383ef2eb9cdf0d918c7e3
sarsalomon <s.yolchiyev1@gmail.com>
2021-11-12 06:44:47 +0500

    Update

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 import AddCategory from "./pages/add/AddCategory"
 import AddProduct from "./pages/add/AddProduct"
-import AddService from "./pages/add/AddService"
-import AddSubCategory from "./pages/add/AddSubCategory"
 import AddUser from "./pages/add/AddUser"
 import Auth from "./pages/Auth"
 import Dashboard from "./pages/Dashboard"
@@ -11,16 +9,14 @@ import UpdateCategory from "./pages/update/UpdateCategory"
 import UpdateHistory from "./pages/update/UpdateHistory"
 import UpdateOrder from "./pages/update/UpdateOrder"
 import UpdateProduct from "./pages/update/UpdateProduct"
-import UpdateSubCategory from "./pages/update/UpdateSubCategory"
 import UpdateUser from "./pages/update/UpdateUser"
 import Categories from "./pages/view/Categories"
 import Histories from "./pages/view/Histories"
 import Members from "./pages/view/Members"
 import Orders from "./pages/view/Orders"
 import Products from "./pages/view/Products"
-import Services from "./pages/view/Services"
 import Users from "./pages/view/Users"
-import { ADD_CATEGORY_ROUTE, ADD_PRODUCT_ROUTER, ADD_SERVICE_ROUTE, ADD_SUBCATEGORY_ROUTE, ADD_USER_ROUTER, CATEGORY_ROUTER, DASHBOARD_ROUTER, GET_CATEGORY_ROUTER, GET_HISTORY_ROUTER, GET_ORDER_ROUTER, GET_PRODUCT_ROUTER, GET_SUBCATEGORY_ROUTER, GET_USER_ROUTER, HISTORY_ROUTER, LOGIN_ROUTER, MEMBER_ROUTER, NOTFOUNDED_ROUTER, ORDER_ROUTER, PRODUCT_ROUTER, REGISTRATION_ROUTER, SERVICE_ROUTER, SETTING_ROUTER, USER_ROUTER } from "./utils/consts"
+import { ADD_CATEGORY_ROUTE, ADD_PRODUCT_ROUTER, ADD_USER_ROUTER, CATEGORY_ROUTER, DASHBOARD_ROUTER, GET_CATEGORY_ROUTER, GET_HISTORY_ROUTER, GET_ORDER_ROUTER, GET_PRODUCT_ROUTER, GET_USER_ROUTER, HISTORY_ROUTER, LOGIN_ROUTER, MEMBER_ROUTER, NOTFOUNDED_ROUTER, ORDER_ROUTER, PRODUCT_ROUTER, REGISTRATION_ROUTER, SETTING_ROUTER, USER_ROUTER } from "./utils/consts"
 
 export const authRoutes = [
     {
@@ -30,10 +26,6 @@ export const authRoutes = [
     {
         path: SETTING_ROUTER,
         Component: Setting
-    },
-    {
-        path: SERVICE_ROUTER,
-        Component: Services
     },
     {
         path: PRODUCT_ROUTER,
@@ -68,17 +60,9 @@ export const authRoutes = [
         Component: AddProduct
     },
     {
-        path: ADD_SERVICE_ROUTE,
-        Component: AddService
-    },
-    {
         path: ADD_CATEGORY_ROUTE,
         Component: AddCategory
-    },      
-    {
-        path: ADD_SUBCATEGORY_ROUTE,
-        Component: AddSubCategory
-    },    
+    },         
     {
         path: ADD_USER_ROUTER,
         Component: AddUser
@@ -87,10 +71,6 @@ export const authRoutes = [
         path: GET_CATEGORY_ROUTER + '/:id',
         Component: UpdateCategory
     },    
-    {
-        path: GET_SUBCATEGORY_ROUTER + '/:id',
-        Component: UpdateSubCategory
-    },
     {
         path: GET_ORDER_ROUTER + '/:id',
         Component: UpdateOrder
